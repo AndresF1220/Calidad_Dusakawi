37c7ed5dd82f46928168283d2a551563fe44050a
Andres Fernandez <andresfernandez@dusakawiepsi.com>
2025-10-29 19:41:44 +0000

    Error: ./src/firebase/index.ts:4:1 Export [32mfirebaseConfig[39m doesn

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,6 @@
 
-// 🔧 Configuración principal de tu proyecto Firebase
-// Se han codificado los valores para evitar problemas con las variables de entorno.
 export const firebaseConfig = {
-  apiKey: "AIzaSyCpVj0p_QqN1f1CtyZVJSh-lxwNkbvT8nA",
+  apiKey: "AIzaSyCpVj0p_OqN1f1CtyZVJSh-lxwNkbvT8nA",
   authDomain: "studio-8211110745-23a45.firebaseapp.com",
   projectId: "studio-8211110745-23a45",
   storageBucket: "studio-8211110745-23a45.appspot.com",
