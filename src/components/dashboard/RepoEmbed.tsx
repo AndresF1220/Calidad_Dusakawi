c3b6b67c225ac6f6f06910704ff033341c898738
Andres Fernandez <andresfernandez@dusakawiepsi.com>
2025-11-19 13:34:22 +0000

    Mira, es justo aqui, le doy un clic y no se abre el recuadro para elimin

--- src/components/dashboard/RepoEmbed.tsx
+++ src/components/dashboard/RepoEmbed.tsx
@@ -154,7 +154,7 @@ const FolderTree = ({
                             <Edit className="mr-2 h-4 w-4" />
                             <span>Renombrar</span>
                         </DropdownMenuItem>
-                        <DropdownMenuItem onSelect={(e) => onAction('delete', folder, e)} className="text-destructive">
+                        <DropdownMenuItem onSelect={(e) => onAction('delete', folder, e)} className="text-destructive focus:text-destructive focus:bg-destructive/10">
                             <Trash2 className="mr-2 h-4 w-4" />
                             <span>Eliminar</span>
                         </DropdownMenuItem>
